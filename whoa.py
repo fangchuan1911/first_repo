2faf1b433bfea5497c084615db505a112d834d42
fangchuan1911 <40377308+fangchuan1911@users.noreply.github.com>
2024-09-09 21:31:38 -0400

    Burp program

---
+++ whoa.py
@@ -0,0 +1,6 @@
+def burp(food):
+    if food == "pizza":
+        return f"This {food} is deli--BUUUURRRRRRRPP"
+    else:
+        return f"How delectable is this {food}, and no effect on my gut."
+    
